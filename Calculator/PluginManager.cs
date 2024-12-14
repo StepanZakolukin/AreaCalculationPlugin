6f07acec2f139bad98f620694d9331951d74b671
skwich <a.3066@mail.ru>
2024-12-14 17:14:07 +0500

    Добавил СДК

---
+++ Calculator/PluginManager.cs
@@ -0,0 +1,29 @@
+﻿using AreaCalculationPlugin.Model;
+using AreaCalculationPlugin.View;
+using System.Resources;
+
+using Teigha.Runtime;
+
+namespace AreaCalculationPlugin.Calculator
+{
+    public class PluginManager
+    {
+        [CommandMethod("AreaCalc")]
+        public static void Start()
+        {
+            var defaultAreaCoefficients = GetRoomCoefficients();
+            var mainForm = new AreaOfThePremises(defaultAreaCoefficients);
+            mainForm.Show();
+        }
+
+        private static CoefficientsInfo[] GetRoomCoefficients()
+        {
+            var rm = new ResourceManager(typeof(AreaOfThePremises));
+
+            return rm.GetString("Coefficients").Split("\r\n")
+                .Select(line => line.Split(": "))
+                .Select(array => new CoefficientsInfo(array.First(), double.Parse(array.Last())))
+                .ToArray();
+        }
+    }
+}
